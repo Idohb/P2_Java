2655bc7ae696c6505c1b4907c791b00a31579c9a
Bodivann <bodivann@gmail.com>
2020-04-23 10:42:16 +0200

    decoupage du main : utilisation du main comme initialisation + methode reader et result + utilisation IOException

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,9 +3,11 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0; // initialize to 0
+/*	private static int headacheCount = 0; // initialize to 0
 	private static int rashCount = 0; // initialize to 0
 	private static int pupilCount = 0; // initialize to 0
 	//test git 3
@@ -37,5 +39,33 @@ public class AnalyticsCounter {
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
+	}*/
+
+	// découpage du main
+	public static void main(String[] args) throws IOException{ // Exception must be declared to be thrown
+		AnalyticsCounter analyticsCounter = new AnalyticsCounter();
+		String line = analyticsCounter.reader("symptoms.txt");
+		analyticsCounter.result(line);
 	}
+	// Test première ligne avant de faire appel à GetSymptom
+	public String reader(String fileName) {
+		String line = "none";
+		if (fileName != null) {
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader(fileName));
+				line = reader.readLine();
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return line;
+	}
+
+	public void result(String line) throws IOException{
+		FileWriter writer = new FileWriter("result.out");
+		writer.write(line);
+		writer.close();
+	}
+
 }
