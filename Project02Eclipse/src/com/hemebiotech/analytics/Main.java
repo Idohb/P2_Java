2c7fc1fe29e975560665e3d58237d3bbeb50cab8
KHEK Bodivann <bodivann@gmail.com>
2020-05-06 12:32:14 +0200

    Remove test for empty File symptoms.txt

--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -7,7 +7,7 @@ import java.util.Map;
  * <h1>Project Number 2 for OC</h1>
  * 
  * @author KHEK Bodivann
- * @version : 1.0.1
+ * @version : 1.0.2
  * 
  */
 public class Main {
@@ -15,9 +15,6 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		AnalyticsCounter analyticsCounter = new AnalyticsCounter();
 		Map<String, Integer> countEstablished = analyticsCounter.counter(); // Analyse "symtoms.txt" file
-		if (!countEstablished.isEmpty())
-			analyticsCounter.outputFile(countEstablished); // Put all occurence in "result.out" file
-		else
-			System.out.println("The symtomps.txt file is empty, so result.out is not written");
+		analyticsCounter.outputFile(countEstablished); // Put all occurence in "result.out" file
 	}
 }
